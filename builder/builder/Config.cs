0aa2d2a7f88649075f650490c431fe0ed10386e6
Christian Grasser <christian.grasser@live.de>
2019-10-17 17:54:58 +0200

    - github action as starting point - update NuGet.CommandLine -> NuGet.CommandLine.6.13.2 - add initial appveyor.yml CI config - prepare for boost 1.88.0 - disabled upload of nuget to avoid upload quota exceeded with appveyor - added dummy ApiKey.cs - avoid nuget warnings about: WARNING: NU5125: The 'licenseUrl' element will be deprecated. Consider using the 'license' element instead. WARNING: NU5128: Some target frameworks declared in the dependencies group of the nuspec and the lib/ref folder do not have exact matches in the other location. Consult the list of actions below: - Add a dependency group for native0.0 to the nuspec

--- builder/builder/Config.cs
+++ builder/builder/Config.cs
@@ -6,7 +6,7 @@ namespace builder
     public static class Config
     {
         public static readonly Version Version = 
-            new StableVersion(1, 87, 0);
+            new StableVersion(1, 88, 0);
 
         public static readonly List[] Release =
         {
@@ -40,9 +40,9 @@ namespace builder
             { "vc110", new CompilerInfo("Visual Studio 2012 Update 4") },
             { "vc120", new CompilerInfo("Visual Studio 2013 Update 5") },
             { "vc140", new CompilerInfo("Visual Studio 2015 Update 3") },
-            { "vc141", new CompilerInfo("Visual Studio 2017 15.9.67") },
-            { "vc142", new CompilerInfo("Visual Studio 2019 16.11.41") },
-            { "vc143", new CompilerInfo("Visual Studio 2022 17.11.5") },
+            { "vc141", new CompilerInfo("Visual Studio 2017 15.9.70") },
+            { "vc142", new CompilerInfo("Visual Studio 2019 16.11.44") },
+            { "vc143", new CompilerInfo("Visual Studio 2022 17.13.0") },
         };
 
         public static int CompilerNumber(string key)
